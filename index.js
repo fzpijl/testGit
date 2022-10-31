3ab46e2b79d23f045028b189538ce673144e24f0
fzpijl <624106624@qq.com>
2022-10-31 21:41:41 +0800

    update

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
-const fs = require('fs')
 
-console.log(fs);
+const a = {
+    a: 'aa',
+    b: {
+        b: "bbb"
+    }
+}
+console.error(a)
+
+const s = undefined
+console.log(s);
+
+for (let i = 0; i < 10; i++) {
+    console.log(s)
+    // console.log(s);
+    // console.log(s)
+}
